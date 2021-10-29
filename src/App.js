5cd2e94a6d9d52ee8a1643e56db8e8c0ceef6148
bayali12 <zloo095@yandex.ru>
2021-10-29 14:20:31 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -44,13 +44,25 @@ function App() {
 
   const onAddToCart = async (obj) => {
     try {
-      if(cartItems.find(item => Number(item.id) === Number(obj.id))){
-        setCartItems(prev => prev.filter(item => Number(item.id) !== Number(obj.id)))
-        await axios.delete(`https://6156e726e039a0001725ac90.mockapi.io/cart/${obj.id}`);
+      const findItem = cartItems.find(item => Number(item.parentId) === Number(obj.id));
+      if(findItem){
+        setCartItems(prev => prev.filter(item => Number(item.parentId) !== Number(obj.id)))
+        await axios.delete(`https://6156e726e039a0001725ac90.mockapi.io/cart/${findItem.id}`);
       }
       else {
-        await axios.post('https://6156e726e039a0001725ac90.mockapi.io/cart', obj);
         setCartItems(prev => [...prev,obj]);
+        const {data} = await axios.post('https://6156e726e039a0001725ac90.mockapi.io/cart', obj);
+        setCartItems((prev) =>
+        prev.map((item) => {
+          if (item.parentId === data.parentId) {
+            return {
+              ...item,
+              id: data.id,
+            };
+          }
+          return item;
+        }),
+      );
       }
     } catch (error) {
       alert('Не удалось добавить товар в корзину')
@@ -85,7 +97,7 @@ function App() {
   }
 
   const isItemAdded = (id) =>{
-    return cartItems.some(obj => Number(obj.id) === Number(id))
+    return cartItems.some(obj => Number(obj.parentId) === Number(id))
   }
 
   return (
